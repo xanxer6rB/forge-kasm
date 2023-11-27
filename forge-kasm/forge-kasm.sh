c00d4a7530cac04c1f1cb1211e8759c9d83fef63
xanxer6rB <51731282+xanxer6rB@users.noreply.github.com>
2023-11-26 20:22:13 -0600

    Update forge-kasm.sh

--- forge-kasm/forge-kasm.sh
+++ forge-kasm/forge-kasm.sh
@@ -6,11 +6,11 @@ apt update; sudo apt upgrade -y
 apt install -y wget nano bzip2 jq openjdk-11-jdk
 wait
 cd forge
-wget https://downloads.cardforge.org/dailysnapshots/forge-gui-desktop-1.6.59-SNAPSHOT-11-24.tar.bz2
+wget https://downloads.cardforge.org/dailysnapshots/forge-gui-desktop-1.6.59-SNAPSHOT-11-26.tar.bz2
 echo "EXTRACTING FILES PLEASE WAIT"
-bzip2 -d forge-gui-desktop-1.6.59-SNAPSHOT-11-24.tar.bz2
-tar -xf forge-gui-desktop-1.6.59-SNAPSHOT-11-24.tar
-rm forge-gui-desktop-1.6.59-SNAPSHOT-11-24.tar
+bzip2 -d forge-gui-desktop-1.6.59-SNAPSHOT-11-26.tar.bz2
+tar -xf forge-gui-desktop-1.6.59-SNAPSHOT-11-26.tar
+rm forge-gui-desktop-1.6.59-SNAPSHOT-11-26.tar
 chmod +x *.sh
 echo "DONE"
 echo -e "SETUP COMPLETE. \nUSE COMMAND: \nFORGE-GUI-DESKTOP: ./forge/forge.sh \nFORGE-ADVENTURE: ./forge/forge-adventure.sh"

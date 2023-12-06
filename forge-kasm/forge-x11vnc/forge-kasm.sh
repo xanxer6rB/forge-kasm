0e43c44341d413e40cc6b50720f8e8ad48968cbb
xanxer6rB <51731282+xanxer6rB@users.noreply.github.com>
2023-12-05 21:34:36 -0600

    Add files via upload

---
+++ forge-kasm/forge-x11vnc/forge-kasm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "FORGE-GUI-DESKTOP BUILD SCRIPT TEST - forge-kasm"
+#mkdir forge
+apt update; sudo apt upgrade -y
+apt install -y wget nano bzip2 jq openjdk-11-jdk
+wait
+#cd forge
+wget https://downloads.cardforge.org/dailysnapshots/forge-gui-desktop-1.6.59-SNAPSHOT-11-30.tar.bz2
+echo "EXTRACTING FILES PLEASE WAIT"
+bzip2 -d forge-gui-desktop-1.6.59-SNAPSHOT-11-30.tar.bz2
+tar -xf forge-gui-desktop-1.6.59-SNAPSHOT-11-30.tar
+rm forge-gui-desktop-1.6.59-SNAPSHOT-11-30.tar
+chmod +x *.sh
+echo "DONE"
+echo -e "SETUP COMPLETE. \nUSE COMMAND: \nFORGE-GUI-DESKTOP: ./forge/forge.sh \nFORGE-ADVENTURE: ./forge/forge-adventure.sh"

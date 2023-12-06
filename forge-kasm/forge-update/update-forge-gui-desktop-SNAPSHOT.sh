f9769c00af83c913e6dffc2724a31775950ad261
xanxer6rB <51731282+xanxer6rB@users.noreply.github.com>
2023-12-05 18:37:44 -0600

    Update update-forge-gui-desktop-SNAPSHOT.sh

--- forge-kasm/forge-update/update-forge-gui-desktop-SNAPSHOT.sh
+++ forge-kasm/forge-update/update-forge-gui-desktop-SNAPSHOT.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#rm -rf forge
+rm -rf forge
 mkdir forge
 cd forge
 wget -r -l1 -np "https://downloads.cardforge.org/dailysnapshots/" -A "forge-gui-desktop-*.tar.bz2"
-pwd; ls
+#pwd; ls
 mv downloads.cardforge.org/dailysnapshots/*.tar.bz2 /forge
 rm -rf downloads.cardforge.org/dailysnapshots/old downloads.cardforge.org/ downloads.cardforge.org/dailysnapshots/ downloads.cardforge.org
 echo "EXTRACTING FILES PLEASE WAIT, THIS WILL TAKE SOME TIME..."
